dc429680daa374c0467cb03fef60c3f27af30ad9
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-09-07 23:24:05 +0700

    create server

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/go-playground/validator"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
+	"golang-restful-api/app"
+	"golang-restful-api/controller"
+	"golang-restful-api/helper"
+	"golang-restful-api/repository"
+	"golang-restful-api/service"
+	"net/http"
+)
+
+func main() {
+
+	validate := validator.New()
+	db := app.NewDB()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
