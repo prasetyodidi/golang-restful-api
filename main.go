f8c9b74062161e61896c164a7f4ce063f0febd3e
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-09-10 14:15:10 +0700

    impelement middleware and router

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"github.com/go-playground/validator"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 	"golang-restful-api/app"
 	"golang-restful-api/controller"
 	"golang-restful-api/helper"
+	"golang-restful-api/middleware"
 	"golang-restful-api/repository"
 	"golang-restful-api/service"
 	"net/http"
+
+	"github.com/go-playground/validator"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -20,17 +21,11 @@ func main() {
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
-		Handler: router,
+		Handler: middleware.NewAuthMiddleware(router),
 	}
 
 	err := server.ListenAndServe()
